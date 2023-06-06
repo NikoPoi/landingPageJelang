e826d80df6b3e5a4f7b9bdeff7a17f9006672922
NikoPoi <themobiliowow@gmail.com>
2023-06-06 20:29:26 +0700

    Inisialisasi GitHub

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    items : 5, //10 items above 1000px browser width
+    loop: true,
+    nav: true,
+    autoplay: true,
+    autoplaySpeed:1000,
+    smartSpeed: 1500,
+    autoplayHoverPause: true,
+    dots: false
+})
